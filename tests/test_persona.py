8f9ad48cbed4337f3fb8e9f1771e62b5c6eae303
Manuel Felipe Bello Lopez <m.bello@uniandes.edu.co>
2021-03-13 14:13:07 -0500

    Mezclar Job reversion ejecucion completa 6

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -10,6 +10,7 @@ class PersonaTestCase(unittest.TestCase):
       self.persona2 = Persona(nombre='Diego', edad=22)
       self.persona3 = Persona(nombre='Alejandra', edad=25)
       self.persona4 = Persona(nombre='Diana', edad=25)
+      #Print()
       self.grupo = [self.persona1, self.persona2, self.persona3]
 
    def test_constructor(self):
